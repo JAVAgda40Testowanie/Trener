21cb77ffd0308f00cc9c168b8e092d69a7cf81e5
KrzysztofDolanski <k.dolanski@wp.pl>
2020-04-22 18:50:43 +0200

    test comitu

---
+++ src/test/java/pl/sda/tictactoe/board/BoardTest.java
@@ -0,0 +1,24 @@
+package pl.sda.tictactoe.board;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BoardTest {
+
+    @Test
+    void newBoard() {
+    }
+
+    @Test
+    void move() {
+    }
+
+    @Test
+    void draw() {
+    }
+
+    @Test
+    void isGameFinished() {
+    }
+}
