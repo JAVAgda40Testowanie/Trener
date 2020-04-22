48946f2738c7dffcfb342c0c20c7ee182ec61116
KrzysztofDolanski <k.dolanski@wp.pl>
2020-04-22 19:14:17 +0200

    test comitu3

---
+++ src/test/java/pl/sda/tictactoe/board/checks/LineCheckTest.java
@@ -0,0 +1,20 @@
+package pl.sda.tictactoe.board.checks;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LineCheckTest {
+
+    @Test
+    void getAllChecks() {
+    }
+
+    @Test
+    void getInitialFieldIndex() {
+    }
+
+    @Test
+    void getStep() {
+    }
+}
