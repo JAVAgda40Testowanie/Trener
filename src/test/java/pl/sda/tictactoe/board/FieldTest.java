2f5bcada3dd8e6f57a15acd71dcab93deb0813ce
KrzysztofDolanski <k.dolanski@wp.pl>
2020-04-22 18:59:28 +0200

    test comitu2

---
+++ src/test/java/pl/sda/tictactoe/board/FieldTest.java
@@ -0,0 +1,20 @@
+package pl.sda.tictactoe.board;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FieldTest {
+
+    @Test
+    void getPosition() {
+    }
+
+    @Test
+    void getContent() {
+    }
+
+    @Test
+    void setContent() {
+    }
+}
